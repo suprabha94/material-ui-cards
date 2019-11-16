531f9fdcc12b337e0d2a017e66fdfe5f585561c4
Glitch (create-react-app-sample) <none>
2019-11-16 15:17:46 +0000

    💓🎻 Checkpoint ./src/App.js:7447639/331

--- src/App.js
+++ src/App.js
@@ -33,7 +33,15 @@ function App() {
       <Grid container spacing={10} style={{padding: '24px'}}>
         {users.map(
           users => {
-            
+            <Grid key={users.id} item xs={12} sm={6} md={4} lg={4} xl={3}>
+              <PplCard 
+                key={users.id} 
+                email={users.email} 
+                firstname={users.first_name}
+                lastname={users.last_name} 
+                avatar={users.avatar}
+              />
+            </Grid>
           }
         )}
       </Grid>
