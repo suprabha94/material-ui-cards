7cf9e0c729b78e0bc9f237b6e5ae55e05af28f47
Glitch (create-react-app-sample) <none>
2019-11-16 14:29:45 +0000

    🖼👩‍❤️‍💋‍👩 Checkpoint ./src/App.js:7447639/482

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React, {useState, useEffect} from 'react';
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import './App.css';
 
 function App() {
@@ -10,7 +19,7 @@ function App() {
       async function fetchData() {
         setUser(
           await fetch('https://reqres.in/api/users')
-            .then(res => res.json)
+            .then(res => res.json())
             .then(res => res.data)
         )
       }
